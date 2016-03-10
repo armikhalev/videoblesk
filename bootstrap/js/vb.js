fe02e6c3bd7bcc6d019a103f28d686cbd8a51da9
Arseny <armikhalev@gmail.com>
2016-03-09 23:02:33 -0500

    Home page works well, work with design is still needed

--- bootstrap/js/vb.js
+++ bootstrap/js/vb.js
@@ -1,3 +1,9 @@
+/*
+* Author: Arseny Mikhalev
+* Descripton: Script for videoblesk.com
+*/
+
+
 (function( $ ) {
     "use strict";
 
@@ -5,11 +11,23 @@
 
         $(document).ready(function(){
 
+            // run check on page load
+            checkSize();
+
+            // run check on resize of the window
+            $(window).resize(checkSize);
+
+            // Flying up effect of text on page load
             $("#header_text").addClass("upper");
             $("#header_text2").addClass("upper");
             $("#btn").addClass("upper");
 
+            // hide poster and button that appears after pause
             $(".poster").hide();
+            $(".pause").hide();
+            $("#seek-bar").hide();
+            $("#volume-bar").hide();
+            $("#mute").hide();
 
             // declare variables
             var video = $("#intro_video").get(0);
@@ -25,12 +43,18 @@
                     $("#header_text2").hide().animate({
       opacity: 0
   });
-                    $(".poster").hide();
                     $("#btn").hide().animate({
       opacity: 0
   });
-                    video.play();
 
+                    $(".poster").hide();
+                    $("nav").hide();
+                    $(".pause").show();
+                    $("#seek-bar").fadeIn();
+                    $("#volume-bar").fadeIn();
+                    $("#mute").fadeIn();
+
+                    video.play();
 
                 }
                 else {
@@ -41,18 +65,93 @@
                       $("#header_text2").show().removeClass("header_text2").addClass("parent_header_text").animate({
         opacity: 1.0
     }, 1500);
-                      $(".poster").show();
                       $("#btn").show().removeClass("btn").addClass("parent_btn").animate({
         opacity: 1.0
     }, 1500);
+                      $(".poster").show();
+                      $("nav").fadeIn();
+                      $(".pause").hide();
+                      $("#seek-bar").hide();
+                      $("#volume-bar").hide();
+                      $("#mute").hide();
+                      
                       video.pause();
 
                 }
 
-            });
+            }); // end of   $(".play").click
 
-        });
+              // custom video controls
 
-      });
+              // Buttons
+              var muteButton = document.getElementById("mute");
+
+              // Sliders
+              var seekBar = document.getElementById("seek-bar");
+              var volumeBar = document.getElementById("volume-bar");
+              var volumeIcon = document.getElementById("volumeIcon");
+
+              // Event listener for the mute button
+              muteButton.addEventListener("click", function() {
+                // toggle glyphicon of volume mute/sound
+                $(this).find('span').toggleClass('glyphicon-volume-up').toggleClass('glyphicon-volume-off');
+
+                if (video.muted == false) {
+                  // Mute the video
+                  video.muted = true;
+
+                } else {
+                  // Unmute the video
+                  video.muted = false;
+
+                }
+              });
+
+              // Event listener for the volume bar
+              volumeBar.addEventListener("change", function() {
+                // Update the video volume
+                video.volume = volumeBar.value;
+              });
+
+              // Event listener for the seek bar
+              seekBar.addEventListener("change", function() {
+                // Calculate the new time
+                var time = video.duration * (seekBar.value / 100);
+
+                // Update the video time
+                video.currentTime = time;
+              });
+
+              // Update the seek bar as the video plays
+              video.addEventListener("timeupdate", function() {
+                // Calculate the slider value
+                var value = (100 / video.duration) * video.currentTime;
+
+                // Update the slider value
+                seekBar.value = value;
+              });
+
+
+              // Pause the video when the slider handle is being dragged
+              seekBar.addEventListener("mousedown", function() {
+                video.pause();
+              });
+
+              // Play the video when the slider handle is dropped
+              seekBar.addEventListener("mouseup", function() {
+                video.play();
+              });
+
+        }); // end of $(document).ready
+
+        //Function to the css media query rules
+        function checkSize(){
+            if ($(".fullscreen-bg__video").css("display") == "none" ){
+                $("#container").removeClass("play");
+            }
+          }
+
+
+      }); // end of $(function()
 
 }(jQuery));
--- home_page.php
+++ home_page.php
@@ -2,9 +2,9 @@
 
 <?php get_header(); ?>
 
-<div class="container play">
+<div id="container" class="container play">
 <div class="row">
-	<div class="box">
+
 
          <div class="col-lg-12 text-center">
 
@@ -15,8 +15,6 @@
 
 							</div>
 
-
-
 							<div id="header_text2" class="header_text2">
 
 									<h3>Saving your dreams on a hard copy!</h3>
@@ -24,25 +22,46 @@
 
 							</div>
 
+						<!-- button that appears when video paused and on page load -->
 				 		<button type="button" id="btn" class="btn">Play video</button>
 
+				 <!-- image that appears after pause , it is not the same source as poster in video  but the same image -->
 				 <div class="poster">
 							<img src="http://localhost/videoblesk/wp-content/uploads/photo/camera.jpg" alt="camera photo">
 				 </div>
 
+				 <!-- make video fullscreen -->
 				 <div class="fullscreen-bg">
-					     <video poster="http://localhost/videoblesk/wp-content/uploads/photo/cameraFront.jpg" id="intro_video" class="fullscreen-bg__video">
+					     <video loop poster="http://localhost/videoblesk/wp-content/uploads/photo/cameraFront.jpg" id="intro_video" class="fullscreen-bg__video">
 
 					         <source src="http://localhost/videoblesk/wp-content/uploads/video/Andrei_Video.mp4" type="video/mp4">
-
+										 <p>
+								       Your browser doesn't support HTML5 video.
+								     </p>
 					     </video>
+
 				  </div>
 
+        </div><!-- col-lg-12 text-center -->
 
-        </div>
-
-    </div><!---- .box --->
 </div><!-- .row -->
-</div>
 
+		 <!-- button that appears with all the controls when video played.
+		 Separated from video controls because doesn't do anything,
+		 only uses play class to toggle play/pause as well as the whole container.
+	   It is needed as an indicator that video can be paused -->
+		 <button class='pause'>II</button>
+
+</div><!-- div class="container play" -->
+
+<!-- Video Controls -->
+<div class="videoControls">
+
+		 <input type="range" id="seek-bar" value="0">
+		 <button type="button" id="mute"><span id="volumeIcon" class="glyphicon glyphicon-volume-up"></span></button>
+		 <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
+
+</div><!-- div class="videoControls" -->
+
+</div>
 <?php get_footer(); ?>
