417f9c729be952d9b3b53df917aee5ed46f7ce0e
Arseny <armikhalev@gmail.com>
2016-03-10 23:10:44 -0500

    Adds About page with initial design and style

---
+++ about_page.php
@@ -0,0 +1,76 @@
+<?php /* Template Name: about_page */ ?>
+
+<?php get_header(); ?>
+<div class="container">
+
+<div class="row-fluid">
+
+           <!-- Background image -->
+           <div class="about_background">
+              <img src="http://localhost/videoblesk/wp-content/uploads/photo/noche.jpg" alt="night background">
+           </div>
+</br>
+           <div class="col-sm-6">
+              <img class="img-responsive img-thumbnail" src="http://localhost/videoblesk/wp-content/uploads/2015/08/ava-1.jpg" alt="Andrei Shaikouski's photo">
+           </div>
+</br>
+           <div class="col-sm-6 about_text text-justify">
+
+               <p><b>Andrei Shaikouski</b></p> 
+               <p>is a videographer with a passion for creativity and artistic expression.</p>
+               <p>Originally from Belarus, he graduated from the Minsk Academy of Arts with a Bachelor's degree in Directing and went on to supervise one of the popular talk shows in Belarus.</p>
+               <p>Today, Andrei is dedicated to preserving happy memories by filming and editing high quality video of your most cherished events!</p>
+
+           </div>
+
+</div><!-- .row -->
+<div class="col-sm-12"></div>
+
+<div class="row-fluid">
+
+<div class="col-sm-6 about_text feedback_text">
+</br>
+           <div class="well">
+                 <p><b>Mark Avrelius</b>
+                 <img class="img-responsive img-circle feedback_icon" src="http://localhost/videoblesk/wp-content/uploads/photo/avrelius.jpg">
+                 </p>
+
+                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+           </div>
+
+           <div class="well">
+                <p><b>Julius Caesarus</b>
+                <img class="img-responsive img-circle feedback_icon" src="http://localhost/videoblesk/wp-content/uploads/photo/cesar.jpg">
+                </p>
+
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+           </div>
+</div>
+<div class="col-sm-6 about_text feedback_text">
+</br>
+           <div class="well">
+                 <p><b>Mark Avrelius</b>
+                 <img class="img-responsive img-circle feedback_icon" src="http://localhost/videoblesk/wp-content/uploads/photo/avrelius.jpg">
+                 </p>
+
+                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+           </div>
+
+           <div class="well">
+                 <p><b>Julius Caesarus</b>
+                 <img class="img-responsive img-circle feedback_icon" src="http://localhost/videoblesk/wp-content/uploads/photo/cesar.jpg">
+                 </p>
+
+                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+           </div>
+</div>
+
+</div><!-- .row -->
+
+</div>
+
+<?php get_footer(); ?>
